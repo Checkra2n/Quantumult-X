22b2815d3089ef50eeb4159e34216a1a59c46d90
托尼老师。 <74044525+anran180224@users.noreply.github.com>
2020-12-28 01:13:56 +0800

    Update PriceTagAD.js

--- PriceTagAD.js
+++ PriceTagAD.js
@@ -1,4 +1,4 @@
-hostname= appfan.im, ssp-x.yoloho.com,
+hostname= appfan.im
 
-#PriceTag 去首页广告
+#PriceTag 去首页广告(appfan.im, ssp-x.yoloho.com)
 ^https?:\/\/appfan\.im\/api\/v2\/topics\/.*?/posts.* url script-response-body https://raw.githubusercontent.com/alpha87/QuantumultX-Profiles/master/PriceTagAD.js
