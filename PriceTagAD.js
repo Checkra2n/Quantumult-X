976cb20fd0d9a5196fcc096dbdb8713e566aacf0
托尼老师。 <74044525+anran180224@users.noreply.github.com>
2021-01-01 22:04:32 +0800

    Delete PriceTagAD.js

--- PriceTagAD.js
+++
@@ -1,4 +0,0 @@
-hostname= appfan.im
-
-#PriceTag 去首页广告(appfan.im, ssp-x.yoloho.com)
-^https?:\/\/appfan\.im\/api\/v2\/topics\/.*?/posts.* url script-response-body https://raw.githubusercontent.com/alpha87/QuantumultX-Profiles/master/PriceTagAD.js
