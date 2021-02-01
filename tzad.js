75b53ca2c40dceb4c7779062b58a83ff301c2510
托尼老师。 <74044525+anran180224@users.noreply.github.com>
2021-02-01 21:02:09 +0800

    Create tzad.js

---
+++ tzad.js
@@ -0,0 +1,20 @@
+/*  
+
+
+QX:
+[rewrite_local]
+#桃子去广告
+^http:\/\/api\.(.+)\.xyz\/fast-cloud\/ads\/fetch url script-response-body taozi.js
+
+[mitm]
+hostname = api.*.xyz,
+
+桃子视频下载地址；
+http://69t.co
+*/
+
+var obj = JSON.parse($response.body);
+obj.data = 0;
+
+
+$done({body: JSON.stringify(obj)}); 
