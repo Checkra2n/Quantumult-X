75102a72d3bb87232a8db951f8d4c835b14b953c
托尼老师。 <74044525+Checkra2n@users.noreply.github.com>
2022-02-03 18:51:34 +0800

    Update Jd_TaoBao.js

--- JS.conf/Jd_TaoBao.js
+++ JS.conf/Jd_TaoBao.js
@@ -1,8 +1,10 @@
-hostname=api.m.jd.com,trade-acs.m.taobao.com
+# By StimeKe (https://github.com/StimeKe)
 
-#京东历史比价 
-^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/jd_price.js
+hostname = api.m.jd.com, trade-acs.m.taobao.com
 
-#淘宝历史比价 
+# 京东比价
+^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
+
+# 淘宝比价
 ^http://.+/amdc/mobileDispatch url script-request-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
 ^https?://trade-acs\.m\.taobao\.com/gw/mtop\.taobao\.detail\.getdetail url script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
